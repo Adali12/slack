2905c033c2dbb5b65938a5ec8a96a7840d17d440
adali12 <abayiringira1994@yahoo.com>
2019-11-14 04:41:46 -0800

    last clockwork chanell notification

--- sample.rb
+++ sample.rb
@@ -1,9 +1,11 @@
-require "slack-notify"
+require "slack-notifier"
 require 'clockwork'
+require 'active_support/time'
 module  Clockwork
-    handler do |portal|
-        puts "Here we are"
+    handler do |send|
+        notifier = Slack::Notifier.new('https://hooks.slack.com/services/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')
+        notifier.ping('good morning')
       end
  
- every(3.seconds, 'portal')
+ every(3.seconds, 'send')
     end 
